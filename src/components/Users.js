b2af2d8c5975492c788c1d2d66587e49cc7e1079
cfernando415 <ca.fernando415@gmail.com>
2019-01-28 10:38:12 -0500

    done

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
 class Users extends Component {
-
   render() {
     return (
       <div>
-        <ul>
-          Users!
-        </ul>
+        <ul>Users!</ul>
       </div>
-    )
+    );
   }
 }
 
 //add mapStateToProps here
-
-export default Users
+const mapStateToProps = state => {
+  return { users: state.users };
+};
+export default connect(mapStateToProps)(Users);
